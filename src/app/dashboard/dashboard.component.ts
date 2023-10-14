e16ec20474f531becf818033397f033b1c9ba964
CarloskarimLS <84647186+CarloskarimLS@users.noreply.github.com>
2023-10-13 18:16:09 -0600

    Update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 
+//Hola
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
