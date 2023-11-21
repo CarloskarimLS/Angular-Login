4f0313ccc00c43571f05c1d19290f961f4367bd3
CarloskarimLS <karimserafin91@aragon.unam.mx>
2023-11-20 22:09:07 -0600

    Update

--- src/app/apis/apis.component.ts
+++ src/app/apis/apis.component.ts
@@ -14,19 +14,10 @@ export class ApisComponent implements OnInit {
     constructor(private http: HttpClient, private router: Router) {}
 
     ngOnInit(): void{
-        debugger;
         this.http.get<any[]>('https://api.openalex.org/works?sample=20').subscribe(
         (response) => {
           this.works = response;
-          console.log(this.works);
-          const itemToFind = "id"; // Reemplaza con el título que deseas buscar
-          const foundItem = this.works.find(item => item.title === itemToFind);
-
-          if (foundItem) {
-            console.log("Se encontró el elemento:", foundItem);
-          } else {
-            console.log("No se encontró ningún elemento con el título especificado.");
-          }
+          console.log(response);
         },
         (error) => {
         console.error('Error al obtener los datos', error);
--- src/app/apis/apis.modelo.ts
+++
@@ -1,6 +0,0 @@
-export interface Frase {
-    value: string; // Contiene la frase
-    id: string;
-    doi: string; // Identificador la frase aleatoria
-    title: string; // URL que contiene el ID y permite recuperar la frase
-  }
