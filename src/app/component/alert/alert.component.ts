7eae102c6f9c981ec6546d3cd39a9ebd05ff2a97
K4r1m9 <carloskarimls@hotmail.com>
2024-01-17 22:07:04 -0600

    Update Object

--- src/app/component/alert/alert.component.ts
+++ src/app/component/alert/alert.component.ts
@@ -3,7 +3,6 @@ import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgFor, NgIf } from '@angular/common';
 import { MyService } from './service.component';
 import { IWorks } from './IWorks';
-import { count } from 'rxjs';
 @Component({
   selector: 'app-ngbd-alert',
   standalone: true,
@@ -21,61 +20,13 @@ import { count } from 'rxjs';
   providers: [MyService]
 })
 export class NgbdAlertBasicComponent implements OnInit  {
-  initialValue: IWorks[] = []; 
-  // this is for the Closeable Alert
+  initialValue: IWorks = {meta: {count: 0, db_response_time_ms: 0, page: 0, per_page: 0, groups_count: null}, results: [], group_by: []}; 
+  constructor(private myService: MyService) { }
 
-  constructor(private myService: MyService) {
-    
-  }
-  // ngOnInit(): void{
-
-  //   this.myService.getData().subscribe((data) => {
-  //     // const initialValue: IWorks = data;
-  //     // this.initialValue = data;
-  //     //   console.log(data);
-  //     //   console.log(this.initialValue);
-  //     //   console.log("initial value: " + initialValue);
-  //     //   console.log(initialValue.meta.count);
-  //     //   console.log(JSON.stringify(this.initialValue));
-  //     this.initialValue = data;
-  //     console.log(data);
-  //     console.log(JSON.stringify(this.initialValue));
-  //   });
-  // }
   ngOnInit(): void {
     this.myService.getData().subscribe(
-      (data: any) => {
+      (data: IWorks) => {
         this.initialValue = data;
-        console.log('Datos asignados:', this.initialValue);
-        const c = {initialValue: []}
-        console.log(c.initialValue)
-        console.log(c.initialValue[0])
-        console.log(data)
-        
-        if(!data){
-          return data;
-        }
-        if(!data.meta) return data;
-
-        if(!data.meta.count) return data;
-        
-        let meta: IWorks = {
-          ...data.meta.count,
-          count:
-            ( data.meta.count &&
-              data.meta.count[0]
-            )
-            ? data.meta.count[0] : '' 
-        }
-
-        //https://stackoverflow.com/questions/69107755/angular-error-typeerror-cannot-read-properties-of-undefined-reading-licence
-        data.meta.count = meta;
-
-        console.log(meta)
-        console.log(data)
-        
-        console.log(data.meta.count)
-        
       },
       (error) => {
         console.error('Error al obtener datos:', error);
@@ -83,10 +34,4 @@ export class NgbdAlertBasicComponent implements OnInit  {
     );
   }
   
-}
-
-export interface IAlert {
-  id: number;
-  type: string;
-  message: string;
 }
