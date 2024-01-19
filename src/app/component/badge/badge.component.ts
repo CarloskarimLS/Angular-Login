43fa3822eeadc97f4c636dfcd6729a484f4ac79e
K4r1m9 <carloskarimls@hotmail.com>
2024-01-19 11:10:02 -0600

    Update UI

--- src/app/component/badge/badge.component.ts
+++ src/app/component/badge/badge.component.ts
@@ -15,8 +15,11 @@ import {MatExpansionModule} from '@angular/material/expansion';
 })
 export class BadgeComponent implements OnInit  {
   initialValue: IAuthors = {meta: {count: 0, db_response_time_ms: 0, page: 0, per_page: 0, groups_count: null}, results: [], group_by: []};
+  initialValueById: IAuthorsByID = {id: '', orcid: null, display_name: '', display_name_alternatives: [], works_count: 0, cited_by_count: 0, summary_stats: { "2yr_mean_citedness": 0, h_index: 0, i10_index: 0}, ids: {openalex: ''}, affiliations: [], last_known_institution: {id: '', ror: '', display_name: '', country_code: '', type: '', lineage: []}, last_known_institutions: [], x_concepts: [], counts_by_year: [], works_api_url: '', updated_date: new Date(), created_date: new Date()};
 
   panelOpenState = false;
+  searchById = false;
+  errorInSearch = false;
   constructor(private serviceAuthors: ServiceAuthors) { }
   @Input() openAlexId: string = '';
 
@@ -24,6 +27,7 @@ export class BadgeComponent implements OnInit  {
     this.serviceAuthors.getData().subscribe(
       (data: IAuthors) => {
         this.initialValue = data;
+        console.log(this.initialValue);
       },
       (error) =>{
         console.log("Error", error);
@@ -32,12 +36,15 @@ export class BadgeComponent implements OnInit  {
   }
 
   searchAuthor(){
-    this.serviceAuthors.getAuthorById('A5040654425').subscribe(
-      (data: any) =>{
-        console.log(data);
+    this.serviceAuthors.getAuthorById(this.openAlexId).subscribe(
+      (data: IAuthorsByID) =>{
+        this.initialValueById = data;
+        this.searchById = true;
+        console.log(this.initialValueById);
       },
       (error) =>{
-        console.log("Error", error);
+        this.errorInSearch = true;
+        this.searchById = false;
       }
     )
   }
--- src/app/component/badge/serviceAuthors.component.ts
+++ src/app/component/badge/serviceAuthors.component.ts
@@ -9,7 +9,7 @@ export class ServiceAuthors {
     constructor(private http: HttpClient){}
 
     getData(): Observable<any> {
-      const url = `https://api.openalex.org/authors`;
+      const url = `https://api.openalex.org/authors?per-page=100&page=1`;
       return this.http.get(url).pipe(
         tap(data => console.log('Service Data:', data))
       );
